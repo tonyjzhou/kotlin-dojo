208b776c832434d51bd5905cdaaacaa54277121d
Tony Zhou <tony.zhou@affirm.com>
2020-12-07 14:46:32 -0800

    Add sample test

---
+++ src/main/kotlin/Calculator.kt
@@ -0,0 +1,1 @@
+fun add(a: Int, b: Int): Int = a + b
---
+++ src/test/kotlin/CalculatorTest.kt
@@ -0,0 +1,9 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class CalculatorTest {
+    @Test
+    fun testAdd() {
+        assertEquals(1, add(1, 0))
+    }
+}
